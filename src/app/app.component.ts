05c0d2d999646d2b29126e23e7b414b693eb34bf
Vidhya Asuri <sasuri@sstire.com>
2019-01-24 11:15:23 -0500

    founfation js and css are working in this version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+declare var $:any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(){};
+  ngOnInit(){
+  $(document).foundation();
+  };
   title = 'ng-foundation-app';
+
 }
