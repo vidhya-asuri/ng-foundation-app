d2da1538b89f77ff8908c265acf364330f6ad746
Vidhya Asuri <sasuri@sstire.com>
2019-02-11 15:51:34 -0500

    change getHeroes method to hero.service to use the HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,21 +5,27 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService} from './message.service';
-
+import {Httpclient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroService {
-  constructor(public messageService: MessageService) { }
-  getHeroes(): Observable<Hero[]>{
-     this.messageService.add('HeroService: fetched heroes');
-     return of(HEROES);
-  }
+  constructor(public http:HttpClient,
+  public messageService: MessageService) { }
+  
+  	getHeroes(): Observable<Hero[]> {
+  		return this.http.get<Hero[]>(this.heroes.Url);
+	}
 
-  getHero(id: number): Observable<Hero>{
-     this.messageService.add(`HeroService: fetched Hero with id=${id}`);
-     return of(HEROES.find(hero=> hero.id === id));
-  }
+  	/** Log a HeroService message with the MessageService */
+	private log(message: string) {
+  		this.messageService.add(`HeroService: ${message}`);
+	}
+
+	getHero(id: number): Observable<Hero>{
+	    this.messageService.add(`HeroService: fetched Hero with id=${id}`);
+	    return of(HEROES.find(hero=> hero.id === id));
+	}
 }
