8496020316d2a1f7f4ae5574cea31a8e8266d26a
Vidhya Asuri <sasuri@sstire.com>
2019-02-11 15:20:21 -0500

    add in-memory-web-api to act as server; this module intercepts HTTP requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component,OnInit } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
+
 declare var $:any
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
@@ -27,7 +29,14 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes) 
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+	// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+	// and returns simulated server responses.
+	// Remove it when a real server is ready to receive requests.
+	HttpClientInMemoryWebApiModule.forRoot(
+  		InMemoryDataService, { dataEncapsulation: false }
+	)
   ],
 
 
